5bbeb9a52d33a4cb61618dbd12199dc7a700a383
reza <rezasalehibabarsad@hotmail.com>
2023-05-02 22:55:17 +0330

    ColdFlow

--- app/src/main/java/com/rezoo/kotlinflows/MainActivity.kt
+++ app/src/main/java/com/rezoo/kotlinflows/MainActivity.kt
@@ -3,13 +3,19 @@ package com.rezoo.kotlinflows
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.rezoo.kotlinflows.ui.theme.KotlinFlowsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +23,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KotlinFlowsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                val viewModel = viewModel<MainViewModel>()
+                val time = viewModel.countDownFlow.collectAsState(initial = 10)
+
+                Box(
+                    modifier = Modifier.fillMaxSize()) {
+                    Text(
+                        text =time.value.toString(),
+                        fontSize = 34.sp,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    KotlinFlowsTheme {
-        Greeting("Android")
     }
 }
